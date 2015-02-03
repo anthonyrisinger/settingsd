9d6be939bc42d09abe5e565e2cf40026e3c93a17
C Anthony Risinger <anthony.risinger@corvisa.com>
2015-02-03 12:39:43 -0600

    bugfix bump to 0.8.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='settingsd',
-    version='0.8.2',
+    version='0.8.3',
     description='settings.d',
     long_description='settings.d',
     url='https://github.com/xtfxme/settingsd',
